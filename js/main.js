a15093b507e059a8dee991d8927c43cf44c956bb
lino117 <ss377909135@gmail.com>
2023-12-07 16:27:16 +0100

    - can modifier user - modifier squeal in working

--- js/main.js
+++ js/main.js
@@ -1,72 +1,210 @@
-//
-//     //JS
-//     layui.use(['element', 'layer', 'util'], function(){
-//     var element = layui.element;
-//     var layer = layui.layer;
-//     var util = layui.util;
-//     var $ = layui.$;
-//
-//     //头部事件
-//     util.event('lay-header-event', {
-//     menuLeft: function(){ // 左侧菜单事件
-//     layer.msg('展开左侧菜单的操作', {icon: 0});
-// },
-//     menuRight: function(){  // 右侧菜单事件
-//     layer.open({
-//     type: 1,
-//     title: '更多',
-//     content: '<div style="padding: 15px;">处理右侧面板的操作</div>',
-//     area: ['260px', '100%'],
-//     offset: 'rt', // 右上角
-//     anim: 'slideLeft', // 从右侧抽屉滑出
-//     shadeClose: true,
-//     scrollbar: false
-// });
-// }
-// });
-// });
-
 
 const BaseUrl = 'http://localhost:3000/social/'
 
 
-showTable = async (res, content)=>{
 
-    for (const Element of res) {
-        var username = $('<p>' + Element.username + '</p>')
-        $(content).append ([username])
-    }
-
-}
-creditRender = async (creditAvailable)=>{
-    var available = $('<td class="grid-container">')
-    $.each(creditAvailable,(key,value)=>{
-        available.append($('<div class="grid-x"> <span class="cell large-5"> ' + value + '</span><input class="cell large-5"></div></td>'))
-    })
-
-    return available
-}
 getUsers = async (res,table)=>{
+
     for (const Element of res) {
         const row = $('<tr>')
-        const username = $('<td>' + Element.username + '</td>')
-        const nickname = $('<td>' + Element.nickname + '</td>')
-        const type = $('<td>' + Element.accountType + '</td>')
+        const username = $('<td id="username">' + Element.username + '</td>')
+        const nickname = $('<td id="nickname">' + Element.nickname + '</td>')
+        const type = $('<td id="type">' + Element.accountType + '</td>')
         const inizialCredit = $('<td>' + Element.creditInit + '</td>')
-
-        const creditHead = '<td class="grid-container"><div class="grid-x"> <span class="cell large-5"> '
-        const creditFeet = '</span><input class="cell large-5" ></div></td>'
-        const dayAvailable = $(creditHead + Element.creditAvailable.daily + creditFeet )
-        const weekAvailable = $(creditHead + Element.creditAvailable.weekly + creditFeet )
-        const monthAvailable = $(creditHead + Element.creditAvailable.monthly + creditFeet )
+        // const form = $('<form action="/" id="changeForm">')
+        const creditHead = '<td class="grid-container number"><div class="grid-x"> <span class="cell large-5"> '
+        const creditFeet = '</span><input class="cell large-5 '
+        const Feet2 = 'value="0" ></div></td>'
+        const dayAvailable = $(creditHead + Element.creditAvailable.daily + creditFeet + 'daily"' + Feet2 )
+        const weekAvailable = $(creditHead + Element.creditAvailable.weekly + creditFeet + 'weekly"' + Feet2)
+        const monthAvailable = $(creditHead + Element.creditAvailable.monthly + creditFeet + 'monthly"' + Feet2)
+        // const closeForm = $('</form>')
         const state = $('<td> libero </td>')
         row.append([username,nickname,type,inizialCredit,dayAvailable,weekAvailable,monthAvailable,state])
         const closeRow = $('</tr>')
-        $(table).append ([row,closeRow])
+        table.append ([row,closeRow])
     }
 }
+getSqueals = async (res,table)=>{
+    for (const squeal of res) {
+        const row = $('<tr>')
+        const username = $('<td id="username">' + squeal.sender + '</td>')
+        const contenuto = $('<td>' + squeal.body + '</td>')
+        const destinatari = $('<td id="destArray">'+squeal.recipients +'</td>')
+        const button = $('<td><a class="button tiny" id="destId"><i class="fi-plus"></i></a></td>')
+        const numTdHead = '<td class="grid-container"><div class="grid-x"> <span class="cell large-5"> '
+        const numTdFeet = '</span><input class="cell large-5" value="0" ></div></td>'
+        // const impression = $(numTdHead + squeal.reaction.impression + numTdFeet)
+        // const like = $(numTdHead + squeal.reaction.impression + numTdFeet)
+        // const dislike = $(numTdHead + squeal.reaction.impression + numTdFeet)
+        const impression = $(numTdHead + 100 + numTdFeet)
+        const like = $(numTdHead + 200 + numTdFeet)
+        const dislike = $(numTdHead + 300 + numTdFeet)
+        const data = $('<td>' + squeal.dateTime + '</td>')
+        // const automatic = $('<td><input type="checkbox" id="automatic" checked=' + squeal.automaticMessage + '></td>')
+        var automatic
+        if (squeal.automaticMessage) {
+            automatic = $('<td><input type="checkbox" id="automatic" checked></td>')
+        }
+        else {
+            automatic = $('<td><input type="checkbox" id="automatic" ></td>')
+        }
+        const closeRow = $('</tr>')
+        row.append([username,contenuto,destinatari,button,impression,like,dislike,data,automatic])
+        table.append([row,closeRow])
+    }
+}
+getChannel = async (res, table)=>{
+    for (const channel of res) {
+        const row = $('<tr>')
+        const name = $('<td>' + channel.name + '</td>')
+        const admin = $('<td>' + channel.admin + '</td>')
+        const desc = $('<td>' + channel.desciption + '</td>')
+        const numFollowers = $('<td>' + channel.followers + '</td>')
+        const numSqueals = $('<td>' + channel.numPost + '</td>')
+        const mutable = $('<td><input type="checkbox" id="automatic"></td>')
+        const closeRow = $('</tr>')
+        row.append([name,admin,desc,numFollowers,numSqueals,mutable])
+        table.append([row,closeRow])
 
-$(document).ready(()=> {
+    }
+}
+ModificUser = async (tbody,username)=>{
+    const inputs = tbody.find('input')
+
+    var userIndex
+    var newUserInfo = []
+    var newCredit = {
+        username : undefined,
+        daily : 0,
+        weekly : 0,
+        monthly : 0
+    }
+
+    inputs.each((index,element)=>{
+        // 判断 input 表格有没有被更改
+        if (element.value != 0){
+            userIndex = parseInt(index/3)
+
+            if (!newCredit.username){
+                newCredit.username=username[userIndex].innerHTML
+            }
+
+            switch ((index+1) % 3){
+                case 1 :
+                    newCredit.daily = (element.value)
+                    break;
+                case 2:
+                    newCredit.weekly = (element.value)
+                    break;
+                case 0:
+                    newCredit.monthly = (element.value)
+                    break;
+            }
+        }
+
+        // 每input数量 等于0 所以等于最后一个 且username存在， 那就push这个用户
+        if ((index+1) % 3 == 0 && newCredit.username){
+            newUserInfo.push(newCredit)
+            newCredit ={
+                username : undefined,
+                daily : 0,
+                weekly : 0,
+                monthly : 0
+            }
+
+        }
+
+    })
+    if (newUserInfo.length!= 0) {
+        $.ajax({
+            contentType: "application/json",
+            data: JSON.stringify(newUserInfo),
+            dataType: "json",
+            method: "PATCH",
+            url: BaseUrl + 'updateUser'
+        }).done((res) => {
+            console.log( 'Modifica eseguita:',res)
+        })
+        //     .always((res)=>{
+        //         console.log(res)
+        // })
+    }
+
+
+}
+
+// ModificSqueal = async (tbody,username)=>{
+//     const inputs = tbody.find('input')
+//     // const checkbox = tbody.find('td[id='']')
+//     var userIndex
+//     var newSquealInfo = []
+//     var newReaction = {
+//         username : undefined,
+//         visitNumber : 0,
+//         likeNumber : 0,
+//         dislikeNumber : 0,
+//         automatic:false
+//     }
+//
+//     inputs.each((index,element)=>{
+//         // 判断 input 表格有没有被更改
+//         if (element.value != 0 || element.type.is('checkbox')){
+//             userIndex = parseInt(index/4)
+//
+//             if (!newReaction.username){
+//                 newReaction.username=username[userIndex].innerHTML
+//             }
+//
+//             switch ((index+1) % 4){
+//                 case 1 :
+//                     newReaction.visitNumber = element.value
+//                     break;
+//                 case 2:
+//                     newReaction.likeNumber = element.value
+//                     break;
+//                 case 3:
+//                     newReaction.dislikeNumber = element.value
+//                     break;
+//                 case 0:
+//                     newReaction.automatic= element.is(':checked')
+//             }
+//         }
+//
+//         // 每input数量 等于0 所以等于最后一个 且username存在， 那就push这个用户
+//         if ((index+1) % 4 == 0 && newReaction.username){
+//
+//             newSquealInfo.push(newReaction)
+//             newReaction = {
+//                 username : undefined,
+//                 visitNumber : 0,
+//                 likeNumber : 0,
+//                 dislikeNumber : 0,
+//                 automatic: false
+//             }
+//
+//         }
+//
+//     })
+//     if (newSquealInfo.length!= 0) {
+//         $.ajax({
+//             contentType: "application/json",
+//             data: JSON.stringify(newUserInfo),
+//             dataType: "json",
+//             method: "PATCH",
+//             url: BaseUrl + 'updateSqueal'
+//         }).done((res) => {
+//             console.log( 'Modifica eseguita:',res)
+//         })
+//
+//     }
+//
+//
+// }
+
+
+
+
 
     $.get(BaseUrl + 'get_all_users', async (res) => {
 
@@ -74,49 +212,96 @@ $(document).ready(()=> {
 
     })
 
-    $('.index').click(async (e) => {
-        // 偶尔会出现bug 比如 unrecognized expression 因为 e 没加载出来
-        const selector = '\#' + e.target.id
-        const content = $(selector).attr('href')
+    $.get(BaseUrl + 'allSqueals', async (res) => {
+        await getSqueals(res, $('#squealTable'))
+    })
 
-        //点击动画 -》 一闪
-        const clicked = $(e.target).parent()
-        clicked.addClass('active')
-        setTimeout(
-            () => {
-                clicked.removeClass('active')
-            },
-            100
-        )
-        // 判断页面 并发送 请求
-        switch (clicked.attr('id')) {
-            case 'users':
-                $.get(BaseUrl + 'get_all_users', async (res) => {
+    // $.get(BaseUrl + 'allChannelP', async (res) => {
+    //     await getChannel(res, $('#privateTable'))
+    // })
+    //
+    // $.get(BaseUrl + 'allChannelO', async (res) => {
+    //     await getChannel(res, $('#officialTable'))
+    // })
+    $('#reset').click( ()=>{
+        $('input').prop({
+            value : 0
+        })
+    })
 
-                    await getUsers(res,$('#userTable'))
-                })
+    $('#change').click(()=>{
+
+        const tbody =  $('.visible').find('tbody')
+        const username = tbody.find("td[id='username']")
+        const dest = tbody.find("td[id='destArray']")
+        console.log(dest)
+        switch (tbody.attr('id')) {
+            case 'userTable':
+                ModificUser(tbody,username)
+                console.log('switch case')
                 break;
-            case 'squeals':
-                $.get(BaseUrl + 'allSqueals', async (res) => {
-
-                })
+            case 'squealTable':
+                ModificSqueal(tbody,username)
                 break;
-            case 'official':
-                $.get(BaseUrl + 'allChannelO', async (res) => {
-
-                })
+            case 'officialTable':
+                ModificOffic(tbody,username)
                 break;
-            case 'private':
-                $.get(BaseUrl + 'allChannelP', async (res) => {
-
-                })
+            case 'privateTable':
+                ModificPrivate(tbody,username)
                 break;
         }
+    })
 
+
+
+
+
+    $('.index').click( (elem) => {
+            const selector = '\#' + elem.target.id
+            const content = $(selector).attr('href')
+
+            //点击动画 -》 一闪
+            const clicked = $(elem.target).parent()
+            clicked.addClass('active')
+            setTimeout(
+                () => {
+                    clicked.removeClass('active')
+                },
+                100
+            )
+        // 初始化 table主体
+        $('tbody').text('')
+
+            // 判断页面 并发送 请求
+            switch (clicked.attr('id')) {
+                case 'users':
+                    $.get(BaseUrl + 'get_all_users', async (res) => {
+
+                        await getUsers(res, $('#userTable'))
+                    })
+                    break;
+                case 'squeals':
+                    $.get(BaseUrl + 'allSqueals', async (res) => {
+                        await getSqueals(res, $('#squealTable'))
+                    })
+
+                    break;
+                case 'official':
+                    $.get(BaseUrl + 'allChannelO', async (res) => {
+                        await getChannel(res, $('#officialTable'))
+                    })
+                    break;
+                case 'private':
+                    $.get(BaseUrl + 'allChannelP', async (res) => {
+                        await getChannel(res, $('#privateTable'))
+                    })
+                    break;
+            }
         // 切换显示画面
-        $('.visible').removeClass('visible').addClass('hide')
+          $('.visible').removeClass('visible').addClass('hide')
 
-        $(content).removeClass('hide').addClass('visible')
+          $(content).removeClass('hide').addClass('visible')
+
 
 
     })
@@ -127,4 +312,3 @@ $(document).ready(()=> {
     //     $('#profileMenu').removeClass('visible').addClass('hide')
     // })
 
-})
