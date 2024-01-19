65d89f7a4cd9ec1e07c5938475678c4673be9f64
lino117 <ss377909135@gmail.com>
2024-01-19 17:57:12 +0100

    - quasi finito la pagina, - si puo migliorare varie parti ove richiesto la modifica dei valori tipo like, credit - manca filtri e richieste arancioni facoltative

--- js/main.js
+++ js/main.js
@@ -2,22 +2,64 @@
 const BaseUrl = 'http://localhost:3000/social/'
 
 addSqueal = async ()=>{
-   // aprire un modal window..... reaveal for fundation take a look !
-   //  $('#addButton').click((e)=>{
-   //      const clicked=$(e.target).parent() //clicked button > parent = td
-   //      $('#channelName').html(name)
-   //  })
-    const tbody = $('#officialTable')
-    $('.addButton').click(async (e)=>{
-        const names = tbody.find("td[id='name']")
-        const index = $(e.target).closest('tr').index()
-        $('#channelName').text(names[index].innerHTML)
 
-        $.get(BaseUrl + 'allSqueals', async (res) => {
-            await getSqueals(res, $('#channelSquealTable'))
+    const tbody = $('#officialTable')
+
+    $('.addButton').click(async (elem)=>{
+        const names = tbody.find("td[id='name']")
+        const index = $(elem.target).closest('tr').index()
+        const channelName = names[index].innerHTML
+        $('#channelName').text(channelName)
+        $.get(BaseUrl + 'channelSqueal_get',{channelName : channelName}, async (res) => {
+            await getChannelSqueals(res, $('#channelSquealTable'))
         })
     })
 
+}
+
+getChannelSqueals = async (res,table)=>{
+    table.text('')
+    for (const squeal of res) {
+            const row = $('<tr>')
+            const username = $('<td id="username">' + squeal.sender + '</td>')
+            const contenuto = $('<td>' + squeal.body + '</td>')
+            const destinatari = $('<td>' + squeal.recipients + '</td>')
+            const impression = $('<td>' + squeal.reaction.impression+'</td>')
+            const like = $('<td>' + squeal.reaction.like+'</td>')
+            const dislike = $('<td>' + squeal.reaction.dislike+'</td>')
+            const data = $('<td>' + squeal.dateTime + '</td>')
+            const squealChannel = $('<td>'+squeal.squealerChannels+'</td>')
+            var automatic
+            if (squeal.automaticMessage) {
+                automatic = $('<td><input type="checkbox" id="automatic" checked></td>')
+            } else {
+                automatic = $('<td><input type="checkbox" id="automatic" ></td>')
+            }
+            const squealId = $('<td id="squealID">' + squeal._id + '</td>')
+            const closeRow = $('</tr>')
+            row.append([username, contenuto, destinatari, impression, like, dislike, data, automatic, squealId])
+            table.append([row, closeRow])
+        }
+
+}
+
+getDelChannel = async (res,table)=>{
+    table.text('')
+    for (const channel of res) {
+        const row = $('<tr>')
+        const checkbox = $('<td><input type="checkbox" class="delCheckbox">')
+        const name = $('<td id="name">' + channel.name + '</td>')
+        const admin = $('<td>' + channel.admin + '</td>')
+        const desc = $('<td>' + channel.description + '</td>')
+        const numFollowers = $('<td>' + channel.followers + '</td>')
+        const numSqueals = $('<td>' + channel.numPost + '</td>')
+        const channelID=$('<td id="delChannelID">'+channel._id+'</td>')
+        const closeRow = $('</tr>')
+
+        row.append([checkbox,name,admin,desc,numFollowers,numSqueals,channelID])
+        table.append([row,closeRow])
+
+    }
 }
 
 getUsers = async (res,table)=>{
@@ -42,23 +84,19 @@ getUsers = async (res,table)=>{
         table.append ([row,closeRow])
     }
 }
+
 getSqueals = async (res,table,squealID)=>{
     for (const squeal of res) {
         const row = $('<tr>')
         const username = $('<td id="username">' + squeal.sender + '</td>')
         const contenuto = $('<td>' + squeal.body + '</td>')
-        // const destinatari = $('<td id="destArray">'+squeal.recipients +'</td>')
-
-        // const button = $('<td id="destButton"><a class="button tiny" id="destId"><i class="fi-plus"></i></a></td>')
         const numTdHead = '<td class="grid-container"><div class="grid-x"> <span class="cell large-5"> '
         const numTdFeet = '</span><input class="cell large-5" value="0" ></div></td>'
         const destinatari = $(numTdHead+squeal.recipients +'</span><input class="cell large-5" type="text" value="'+squeal.recipients+'"></div> </td>')
         const impression = $(numTdHead + squeal.reaction.impression + numTdFeet)
         const like = $(numTdHead + squeal.reaction.like + numTdFeet)
         const dislike = $(numTdHead + squeal.reaction.dislike + numTdFeet)
-        // const impression = $(numTdHead + 100 + numTdFeet)
-        // const like = $(numTdHead + 200 + numTdFeet)
-        // const dislike = $(numTdHead + 300 + numTdFeet)
+
         const data = $('<td>' + squeal.dateTime + '</td>')
         // const automatic = $('<td><input type="checkbox" id="automatic" checked=' + squeal.automaticMessage + '></td>')
         var automatic
@@ -83,24 +121,24 @@ getChannel = async (res, table)=>{
         var desc
         var button
         if (channel.typeOf==='private'){
-            desc = $('<td>' + channel.desciption + '</td>')
+            desc = $('<td>' + channel.description + '</td>')
             button =  $('<td class="hide"><a class="button tiny"><i class="fi-plus"></i></a>')
         }else{
-            desc= $('<td class="grid-container"><div class="grid-x"> <span class="cell large-5">'+channel.desciption+'</span><input class="cell large-5" type="text"></div> </td>')
+            desc= $('<td class="grid-container"><div class="grid-x"> <p class="cell large-5" id="channelDescription">'+channel.description+'</p><input class="cell large-5" type="text"></div> </td>')
             button = $('<td><a class="button tiny addButton" id="addButton" onclick="addSqueal()" data-open="channelSqueals">+</a>')
-            // button = $('<td><a class="button tiny " id="addButton" onclick="'+ await addSqueal(channel.name) +'" data-open="channelSqueals"><i class="fi-plus"></i></a>')
         }
         const numFollowers = $('<td>' + channel.followers + '</td>')
         const numSqueals = $('<td>' + channel.numPost + '</td>')
-        const mutable = $('<td><input type="checkbox" id="automatic"></td>')
-
+        const mutable = $('<td>'+channel.isUnmuteable+'</td>')
+        const channelID=$('<td>'+channel._id+'</td>')
         const closeRow = $('</tr>')
 
-        row.append([name,admin,desc,numFollowers,numSqueals,mutable,button])
+        row.append([name,admin,desc,numFollowers,numSqueals,mutable,button,channelID])
         table.append([row,closeRow])
 
     }
 }
+
 modificUser = async (tbody, username)=>{
     const inputs = tbody.find('input')
 
@@ -115,7 +153,7 @@ modificUser = async (tbody, username)=>{
 
     inputs.each((index,element)=>{
         // 判断 input 表格有没有被更改
-        if (element.value != 0){
+        if (element.value !== 0){
             userIndex = parseInt(index/3)
 
             if (!newCredit.username){
@@ -136,7 +174,7 @@ modificUser = async (tbody, username)=>{
         }
 
         // 每input数量 等于0 所以等于最后一个 且username存在， 那就push这个用户
-        if ((index+1) % 3 == 0 && newCredit.username){
+        if ((index+1) % 3 === 0 && newCredit.username){
             newUserInfo.push(newCredit)
             newCredit ={
                 username : undefined,
@@ -148,7 +186,7 @@ modificUser = async (tbody, username)=>{
         }
 
     })
-    if (newUserInfo.length!= 0) {
+    if (newUserInfo.length!== 0) {
         $.ajax({
             contentType: "application/json",
             data: JSON.stringify(newUserInfo),
@@ -156,11 +194,10 @@ modificUser = async (tbody, username)=>{
             method: "PATCH",
             url: BaseUrl + 'updateUser'
         }).done((res) => {
-            console.log( 'Modifica eseguita:',res)
+            alert( 'Modifica eseguita su : '+res)
+            showPage('usersPanel')
         })
-        //     .always((res)=>{
-        //         console.log(res)
-        // })
+
     }
 
 
@@ -241,7 +278,8 @@ modificSqueal = async (tbody)=>{
             method: "PATCH",
             url: BaseUrl + 'updateSqueal'
         }).done((res) => {
-            console.log( 'Modifica eseguita:',res)
+            alert( 'Modifica eseguita')
+            showPage('squealsPanel')
         })
 
     }
@@ -249,44 +287,188 @@ modificSqueal = async (tbody)=>{
 
 }
 
-modificOff = async (tbody)=>{
-    const inputs = tbody.find('input')
-    const names = tbody.find("td[id='name']")
-    var addSqueals
-    var newDesc
+modificOfficial = async (tbody)=>{
+    var updateChannel=[]
+    var newDescription = {
+        channelName : undefined,
+        description:undefined
+    }
 
-    for (const input of inputs) {
+    $('#officialTable > tr').each(async (index,tr)=>{
+        const newDesc = $(tr).find("input").val()
+        if (newDesc){
+            newDescription.channelName = $(tr).find("td[id='name']").text()
+            newDescription.description = newDesc
+            updateChannel.push(newDescription)
+        }
+    })
+
+    if(updateChannel.length!==0){
+        $.ajax({
+            contentType: "application/json",
+            data: JSON.stringify(updateChannel),
+            dataType: "json",
+            method: "PATCH",
+            url: BaseUrl + 'updateChannelOff'
+        }).done((res) => {
+            alert( 'Modifica eseguita su : ' + res)
+            showPage('officialPanel')
+
+        })
+    }
 
 
+
+}
+
+showPage=async (showPage)=>{
+    // 初始化 table主体
+    $('tbody').text('')
+    // 判断页面 并发送 请求
+    switch (showPage) {
+        case 'users' :
+        case 'usersPanel':
+            $.get(BaseUrl + 'get_all_users', {showNumber : $('#showUserNumber').val()}, async (res) => {
+                await getUsers(res, $('#userTable'))
+            })
+            break;
+
+        case 'squeals':
+        case 'squealsPanel':
+            $.get(BaseUrl + 'allSqueals', {showNumber : $('#showSquealNumber').val()},async (res) => {
+                await getSqueals(res, $('#squealTable'))
+            })
+            break;
+
+        case 'official':
+        case 'officialPanel':
+            $.get(BaseUrl + 'allChannelO', {showNumber : $('#showOffNumber').val()},async (res) => {
+                await getChannel(res, $('#officialTable'))
+            })
+            break;
+        case 'private':
+        case 'privatePanel':
+
+            $.get(BaseUrl + 'allChannelP',{showNumber : $('#showPrivNumber').val()}, async (res) => {
+                await getChannel(res, $('#privateTable'))
+            })
+            break;
     }
 }
 
+modRegisterUserType =  (type)=>{
+    var tipo=undefined
+    switch (type) {
+        case 'Normale':
+            tipo = 'nor'
+            break;
+        case 'Moderatore':
+            tipo = 'mod'
+            break;
+        case 'SMM':
+            tipo = 'smm'
+            break;
+        case 'VIP':
+            tipo = 'vip'
 
 
+    }
+    return tipo
+}
+
+showFiltPage= async (table)=>{
+
+    switch (table) {
+        case 'userTable' :
+            const filter ={
+                showNumber : parseInt($('#showUserNumber').val()),
+                filterName :$('#filtName').val(),
+                filtRule : $('#popFilterRule').val(),
+                filtPop : parseInt($('#popFilterNumber').val()),
+                filtType: modRegisterUserType($('#filtType').val())
+            }
+            $.get(BaseUrl + 'get_filt_users', {filter}, async (res) => {
+                await getUsers(res, $('#'+table))
+            })
+            break;
+
+        // case 'squealTable':
+
+        //     $.get(BaseUrl + 'allSqueals', {showNumber : $('#showSquealNumber').val()},async (res) => {
+        //         await getSqueals(res, $('#squealTable'))
+        //     })
+        //     break;
+        //
+        // case 'officialTable':
+
+        //     $.get(BaseUrl + 'allChannelO', {showNumber : $('#showOffNumber').val()},async (res) => {
+        //         await getChannel(res, $('#officialTable'))
+        //     })
+        //     break;
+        // case 'privateTable':
+
+        //
+        //     $.get(BaseUrl + 'allChannelP',{showNumber : $('#showPrivNumber').val()}, async (res) => {
+        //         await getChannel(res, $('#privateTable'))
+        //     })
+        //     break;
+    }
+}
 $(document).ready(()=>{
 
     $(document).foundation()
 
-    $.get(BaseUrl + 'get_all_users', async (res) => {
+    $.get(BaseUrl + 'get_all_users',{showNumber : $('#showUserNumber').val()}, async (res) => {
 
         await getUsers(res,$('#userTable'))
 
     })
 
-    // $.get(BaseUrl + 'allSqueals', async (res) => {
-    //     await getSqueals(res, $('#squealTable'))
-    // })
-
-    // $.get(BaseUrl + 'allChannelP', async (res) => {
-    //     await getChannel(res, $('#privateTable'))
-    // })
-    //
-    // $.get(BaseUrl + 'allChannelO', async (res) => {
-    //     await getChannel(res, $('#officialTable'))
-    // })
     $('#reset').click( ()=>{
         $('input').prop({
-            value : 0
+            value : ''
+        })
+    })
+
+    $('.refreshpage').click(()=>{
+        const panel = $('.visible').attr('id')
+        showPage(panel)
+    })
+
+    $('#AggiungiSquealChannel').click(()=> {
+        const squealID = $('#addedSquealID').val()
+        // location.reload()
+        // const names = $('#officialTable').find("td[id='name']")
+        // const index = $(elem.target).closest('tr').index()
+
+        const channelName = $('#channelName').text()
+        $.ajax({
+            contentType: "application/json",
+            data: JSON.stringify({squealID: squealID, channelName: channelName}),
+            dataType: "json",
+            method: "PATCH",
+            url: BaseUrl + 'addSquealChannel'
+        }).done((res) => {
+
+            $.get(BaseUrl + 'channelSqueal_get',{channelName : channelName}, async (res) => {
+                await getChannelSqueals(res, $('#channelSquealTable'))
+            })
+        })
+    })
+
+    $('#EliminaSquealChannel').click(()=> {
+        const squealID = $('#addedSquealID').val()
+        const channelName = $('#channelName').text()
+        $.ajax({
+            contentType: "application/json",
+            data: JSON.stringify({squealID: squealID}),
+            dataType: "json",
+            method: "PATCH",
+            url: BaseUrl + 'delSquealChannel'
+        }).done((res) => {
+            $.get(BaseUrl + 'channelSqueal_get',{channelName : channelName}, async (res) => {
+                await getChannelSqueals(res, $('#channelSquealTable'))
+            })
         })
     })
 
@@ -304,7 +486,7 @@ $(document).ready(()=>{
                 modificSqueal(tbody)
                 break;
             case 'officialTable':
-                ModificOffic(tbody,username)
+                modificOfficial(tbody)
                 break;
             case 'privateTable':
                 ModificPrivate(tbody,username)
@@ -312,8 +494,111 @@ $(document).ready(()=>{
         }
     })
 
+    $('#modRegister').click(()=>{
+        const type = modRegisterUserType($('#modRegType').val())
+        const newUser = {
+             username : $('#modRegUsername').val().toLowerCase(),
+             password : $('#modRegPassword').val().toLowerCase(),
+             userType : type,
+             creditInit : $('#modRegCredit').val()
+        }
+        console.log(newUser.userType,'type qui')
+        if (newUser.username && newUser.password && newUser.creditInit){
+            $.post(BaseUrl + 'register',{
+                username:newUser.username,
+                password:newUser.password,
+                userType:newUser.userType,
+                creditInit: newUser.creditInit
+            },async (res)=>{
+                alert('Registrazione effettuata')
+                $('#creatUserForm').foundation('close')
+                showPage('usersPanel')
+
+            })
+
+        }
+    })
+
+    $('#modCreatOff').click(()=>{
+        const newChannel ={
+            name : $('#modOffName').val().toUpperCase(),
+            desc : $('#modOffDesc').val(),
+            type:$('#modOffType').val(),
+        }
+
+        if (newChannel.name && newChannel.desc){
+            $.post(BaseUrl + 'createCh',{
+                name: '§' + newChannel.name ,
+                type : newChannel.type,
+                desc:newChannel.desc
+            }, async (res)=>{
+                $('#creatChannelForm').foundation('close')
+                showPage('officialPanel')
+            })
+        }
+    })
+
+    $('#delChannel').click(()=>{
+        $.get(BaseUrl + 'allChannelO', {showNumber : $('#showOffNumber').val()},async (res) => {
+            await getDelChannel(res, $('#delChannelTable'))
+        })
+
+        $('#delChButton').click(()=>{
+            var delChIDs = []
+            $('#delChannelTable > tr').each(async (index,tr)=>{
+                if ($(tr).find('input').prop('checked')){
+                    delChIDs.push($(tr).find("td[id='delChannelID']").text())
+                }
+            })
+
+            if (delChIDs.length !== 0){
+                $.ajax({
+                    contentType: "application/json",
+                    data: JSON.stringify(delChIDs),
+                    dataType: "json",
+                    method: "PUT",
+                    url: BaseUrl + 'deleteChannel'
+                }).done((res) => {
+                    alert( 'I seguenti CANALI vengono elieminati :'+res)
+                    $('#deleteCh').foundation('close')
+                    showPage('officialPanel')
+
+                })
 
 
+
+
+            }
+
+
+        })
+    })
+
+    // $('#delChannelTable > tr').each(async (index,tr)=>{
+    //     tr.click(async ()=>{
+    //         $(tr).find("input").prop('checked',true)
+    //     })
+    // })
+
+    $('.filterButton').click(async ()=>{
+        const tbody = $('.visible').find('tbody')
+        tbody.text('')
+        showFiltPage(tbody.attr('id'))
+    })
+
+    $('.cleanFiltButton').click(async (e)=>{
+        const filterField = $(e.target).parent().parent()
+        filterField.find('input').each(async (index,input)=>{
+            if (input.type ==='text'){
+                input.value = ''
+            }else {
+                input.value = 0
+            }
+        })
+        filterField.find('select').each(async (index,select)=>{
+            select.selectedIndex = 0
+        })
+    })
 
     $('.index').click( (elem) => {
             const selector = '\#' + elem.target.id
@@ -328,35 +613,7 @@ $(document).ready(()=>{
                 },
                 100
             )
-        // 初始化 table主体
-        $('tbody').text('')
-
-            // 判断页面 并发送 请求
-            switch (clicked.attr('id')) {
-                case 'users':
-                    $.get(BaseUrl + 'get_all_users', async (res) => {
-
-                        await getUsers(res, $('#userTable'))
-                    })
-                    break;
-                case 'squeals':
-                    $.get(BaseUrl + 'allSqueals', async (res) => {
-                        await getSqueals(res, $('#squealTable'))
-                    })
-
-
-                    break;
-                case 'official':
-                    $.get(BaseUrl + 'allChannelO', async (res) => {
-                        await getChannel(res, $('#officialTable'))
-                    })
-                    break;
-                case 'private':
-                    $.get(BaseUrl + 'allChannelP', async (res) => {
-                        await getChannel(res, $('#privateTable'))
-                    })
-                    break;
-            }
+        showPage(clicked.attr('id'))
         // 切换显示画面
           $('.visible').removeClass('visible').addClass('hide')
 
@@ -365,10 +622,7 @@ $(document).ready(()=>{
 
 
     })
+
 })
-    // $('#profile').on('click',async ()=>{
-    //     $('#profileMenu').removeClass('hide').addClass('visible')
-    // }).on('mouseleave',async ()=>{
-    //     $('#profileMenu').removeClass('visible').addClass('hide')
-    // })
+
 
